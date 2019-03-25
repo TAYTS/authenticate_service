eb8fef3fbcccd58a3441617715df47dd22c6ae73
TAY TS <taytzushieh@gmail.com>
2019-03-25 21:37:23 +0800

    feature: Add helper method to test_base class for login with testing account

--- app/tests/test_base.py
+++ app/tests/test_base.py
@@ -1,13 +1,18 @@
 from flask_testing import TestCase
+from flask import url_for
 from app import make_app
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash
-import hashlib
 from datetime import datetime
+import hashlib
+import json
 
 # Import database models
 from models.db import db
 from models.users import Users
+
+# Import helper functions
+from app.tests.utils import get_cookie_from_response
 
 
 class UserUnitTest(TestCase):
@@ -33,6 +38,45 @@ class UserUnitTest(TestCase):
         )
         db.session.add(user)
         db.session.commit()
+
+    def login_with_valid_credential(self):
+        # Get the JWT token cookie
+        response = self.client.post(
+            url_for("users.login"),
+            data=json.dumps({
+                "username": "testing1@gmail.com",
+                "password": "password"
+            }),
+            content_type="application/json"
+        )
+
+        # Define the required cookie names
+        csrf_token_name = "csrf_access_token"
+        access_token_name = "access_token_cookie"
+
+        # Extract the access token cookie
+        access_token = get_cookie_from_response(
+            response,
+            access_token_name
+        )
+
+        # Set test client access_token_cookie
+        self.client.set_cookie(
+            server_name=access_token["domain"],
+            key=access_token_name,
+            value=access_token[access_token_name],
+            path=access_token["path"])
+
+        # Extract the csrf_access_token
+        csrf_token = get_cookie_from_response(
+            response,
+            csrf_token_name
+        )[csrf_token_name]
+
+        # Create the header for csrf token
+        self.csrf_headers = {
+            "X-CSRF-TOKEN": csrf_token
+        }
 
     def setUp(self):
         self.app = self.create_app()
