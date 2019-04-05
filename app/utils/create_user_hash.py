2addcc80bd39b29b308c2ea486e66b4d8d6b7e6b
TAY TS <taytzushieh@gmail.com>
2019-04-06 00:55:50 +0800

    feature: Reduce the hashing length for id_user_hash

--- app/utils/create_user_hash.py
+++ app/utils/create_user_hash.py
@@ -3,6 +3,6 @@ import hashlib
 
 def create_user_hash(username):
     unhashed_user = "email: " + username + "ESC-Accenture"
-    hashed_user = hashlib.sha512(unhashed_user.encode("UTF-8")).hexdigest()
+    hashed_user = hashlib.sha256(unhashed_user.encode("UTF-8")).hexdigest()
 
     return hashed_user
