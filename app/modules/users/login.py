3cbb75aa50e2487fbb7eb2258c750badbf33acb0
Kenneth Sim <kensim28@hotmail.com>
2019-04-13 22:25:12 +0800

    feature: Added reCAPTCHA verification with frontend via login

--- app/modules/users/login.py
+++ app/modules/users/login.py
@@ -1,4 +1,5 @@
 from flask import request, jsonify, current_app
+
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -9,7 +10,8 @@ from flask_jwt_extended import (
 # Import database models
 from models.db import db
 from models.users import Users
-
+# Import for recaptcha post request
+import requests
 
 def login():
     """
@@ -21,36 +23,41 @@ def login():
     """
     # Get the data from the request JSON
     email = str(request.json.get("email"))
+    recaptchatoken = str(request.json.get("recaptchaToken"))
     password = str(request.json.get("password"))
     remember = request.json.get("remember")
 
     # Define return message
     message = {"id_user_hash": ""}
-
     # Check if the data is present
-    if email and password:
-        user = db.session.query(Users).filter(Users.email == email).first()
-        if user:
-            if user.check_password(password):
-                access_token = create_access_token(
-                    identity=user.id_user_hash, fresh=True
-                )
-                refresh_token = create_refresh_token(
-                    identity=user.id_user_hash)
+    if email and password and recaptchatoken:
+        payload = {"secret": "6Lf5-Z0UAAAAAHiCM-F_CGSs7L736pReIuV2LAtr", "response": recaptchatoken}
+        url = "https://www.google.com/recaptcha/api/siteverify"
+        r = requests.get(url, params=payload)
+        if r.json()["success"]:
+            user = db.session.query(Users).filter(Users.email == email).first()
+            if user:
+                if user.check_password(password):
+                    access_token = create_access_token(
+                        identity=user.id_user_hash, fresh=True
+                    )
+                    refresh_token = create_refresh_token(
+                        identity=user.id_user_hash)
 
-                if remember:
-                    access_expire = current_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
-                    refresh_expire = current_app.config["JWT_REFRESH_TOKEN_EXPIRES"]
-                else:
-                    access_expire = None
-                    refresh_expire = None
+                    if remember:
+                        access_expire = current_app.config["JWT_ACCESS_TOKEN_EXPIRES"]
+                        refresh_expire = current_app.config["JWT_REFRESH_TOKEN_EXPIRES"]
+                    else:
+                        access_expire = None
+                        refresh_expire = None
 
-                message["id_user_hash"] = user.id_user_hash
+                    message["id_user_hash"] = user.id_user_hash
 
-                resp = jsonify(message)
-                set_access_cookies(resp, access_token, max_age=access_expire)
-                set_refresh_cookies(resp, refresh_token,
-                                    max_age=refresh_expire)
-                return resp, 200
+                    resp = jsonify(message)
+                    set_access_cookies(resp, access_token, max_age=access_expire)
+                    set_refresh_cookies(resp, refresh_token,
+                                        max_age=refresh_expire)
+                    return resp, 200
+        return jsonify("Failed reCAPTCHA."), 401
 
     return jsonify(message), 401
